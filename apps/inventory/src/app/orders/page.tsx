82d8c44b46fc8e2612beeebf07da1a912b935a55
vc4u2c <vc4u2c@gmail.com>
2024-05-09 14:25:20 -0500

    fix: Align table header

--- apps/inventory/src/app/orders/page.tsx
+++ apps/inventory/src/app/orders/page.tsx
@@ -8,7 +8,7 @@ export interface OrdersProps {}
 export default function Orders(props: OrdersProps) {
   return (
     <div className="py-4 border-muted rounded-lg flex flex-col items-center text-center">
-      <PageTitle title="Orders" />
+      <PageTitle title="Orders" className='pb-2' />
       <OrderList name="Order List"/>
     </div>
   );
