f11d50a3f01bb0b7a3f69cd8729c14d7a187a81c
vc4u2c <vc4u2c@gmail.com>
2024-05-13 20:52:39 -0500

    fix: Title changed

--- libs/orders/src/index.ts
+++ libs/orders/src/index.ts
@@ -1,7 +1,4 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
 
-export { OrderList, OrderListProps } from './lib/order-list/order-list';
-export {
-  OrderDetails,
-  OrderDetailsProps,
-} from './lib/order-details/order-details';
+export * from './lib/order-list/order-list';
+export * from './lib/order-details/order-details';
